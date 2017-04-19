204b0e5fa7ebb25ec5683ff2703ac9b6e642a7be
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2017-04-18 22:49:07 -0700

    Update to express

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
 // server.js
 "use strict";
+/*
+	Adding Express
+*/
+var express = require("express"),
+	port = 3000,
+	path = require("path"),
+	bodyParser = require("body-parser"),
+	app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+// for static content
+app.use(express.static(path.join(__dirname, './assets')));
+app.use(express.static(path.join(__dirname, './bower_components')));
+// setting views
+app.set('views', path.join(__dirname, './client/views'));
+app.set('view engine', 'ejs');
+// routes
+app.get('/', (req, res) => {
+	console.log(req);
+	console.log('*'.repeat(10));
+	console.log(res);
+	res.render('index', {title: 'Simple examlpe of Venmo API'});
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on ${port}`);
+});
+
+/*
+	Node simple server
+*/
+/*
 const http = require("http"),
 	port = 3000,
 	hostname = "127.0.0.1",
@@ -32,25 +64,26 @@ const http = require("http"),
 			});
 			
 		} else if (req.url === "/assets/css/style.css") {
-        fs.readFile('assets/css/style.css', 'utf8', (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, {'Content-type': 'text/css'});
-            res.write(data); 
-            res.end();
-        });
-    }  else if (req.url === "/bower_components/jquery/dist/jquery.min.js") {
-        fs.readFile('bower_components/jquery/dist/jquery.min.js', 'utf8', (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, {'Content-type': 'text/javascript'});
-            res.write(data); 
-            res.end();
-        });
-    } else {
-    	res.writeHead(404);
-    	res.end('File not found!!!');
-    }
+		fs.readFile('assets/css/style.css', 'utf8', (err, data) => {
+			if (err) throw err;
+			res.writeHead(200, {'Content-type': 'text/css'});
+			res.write(data); 
+			res.end();
+		});
+		}  else if (req.url === "/bower_components/jquery/dist/jquery.min.js") {
+			fs.readFile('bower_components/jquery/dist/jquery.min.js', 'utf8', (err, data) => {
+				if (err) throw err;
+				res.writeHead(200, {'Content-type': 'text/javascript'});
+				res.write(data); 
+				res.end();
+			});
+		} else {
+			res.writeHead(404);
+			res.end('File not found!!!');
+		}
 	});
 server.listen(port, hostname, ()=>{
 	console.log(`Server is running on ${hostname}:${port}`);
-});	
+});
+*/
 // "/favicon.ico"
